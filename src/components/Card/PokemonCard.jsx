f3eba8c098667442cb1e76f328ceb428a5201f00
Adam-Brace <adambrace03@gmail.com>
2025-03-03 17:01:19 -0500

    Card

---
+++ src/components/Card/PokemonCard.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+export default function PokemonCard(props) {
+	return (
+		<Card sx={{ maxWidth: 345 }}>
+			<CardMedia
+				sx={{ height: 140 }}
+				image={props.image}
+				title={props.name}
+			/>
+			<CardContent>
+				<Typography gutterBottom variant="h5" component="div">
+					{props.name}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
+}
