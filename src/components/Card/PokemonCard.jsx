3fa6ae165f07b76d2eb0ea25901c0df71ce2ffbe
Adam-Brace <adambrace03@gmail.com>
2025-03-04 10:13:23 -0500

    css refactor
    Co-authored-by: mynameist04st3r <mynameist04st3r@users.noreply.github.com>
    Co-authored-by: marquesj85 <marquesj85@users.noreply.github.com>
    Co-authored-by: Dytzi <Dytzi@users.noreply.github.com>

--- src/components/Card/PokemonCard.jsx
+++ src/components/Card/PokemonCard.jsx
@@ -4,10 +4,11 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
-export default function PokemonCard({props: pokemon}) {
+import "./PokemonCard.css";
+export default function PokemonCard({ props: pokemon }) {
 	return (
-		<Link to = {`/details/${pokemon.id}`} key={pokemon.id}>
-			<Card sx={{ maxWidth: 345 }}>
+		<Link to={`/details/${pokemon.id}`} key={pokemon.id}>
+			<Card className="card" sx={{ maxWidth: 345 }}>
 				<CardMedia
 					sx={{ height: 140 }}
 					image={pokemon.sprites.front_default}
