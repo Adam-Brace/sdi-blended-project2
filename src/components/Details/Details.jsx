c5cd9d3d454f94189d6a476d9f194bab1f0d3030
Marques <marquesj1985@gmail.com>
2025-03-04 13:42:25 -0700

    details initial style

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,39 +2,61 @@ import { useState, useEffect } from "react";
 //import { styled } from "styled-components";
 import PokemonCard from "../Card/PokemonCard";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import {Container, CircularProgress, Stack, Divider} from "@mui/material";
+import "./Details.css"
+
 
 const Details = () => {
+
+const {id} = useParams()
+
+const [pokemon, setPokemon] = useState({});
+
+useEffect(() => {
+	const fetchPokemon = async () => {
+		try{
+	 		const res = await fetch(
+				`https://pokeapi.co/api/v2/pokemon/${id}`
+			);
+			const data = await res.json();
+			setPokemon(data);
+		} catch (error) {
+			console.error("Error fetching Pokémon:", error);
+		}
+	}
+	fetchPokemon()
+
+}, []);
+
+
 	return (
-		<>
-			<div>
+
+		<Container className="mainContainer">
+			<Stack direction="column" divider={<Divider orientation="horizontal" flexItem />} spacing={2}>
+				<div className = "banner">
 				<Link to="/">
 					<button>LogoHome</button>
 				</Link>
-				<h1>Optional Content</h1>
-			</div>
-			<div>
-				<h2>PokemonCard.name</h2>
-			</div>
-			<div>
-				<h2>PokemonCard.img</h2>
-				<h2>Shiny.img</h2>
-			</div>
-			<div>
-				<div>
-					<h3>Type</h3>
 				</div>
-				<div>
-					<h3>Stats</h3>
+				<div className = "identity">
+				<h1> {pokemon.name} </h1>
+				<h1></h1>
 				</div>
-				<h3>Moves</h3>
-			</div>
-			<div>
-				<p>Height, Weight, Color, Flavor Text</p>
-			</div>
-			<div>
-				<h2>EvoLine</h2>
-			</div>
-		</>
+				<div className = "stats">
+					<Stack direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={2}>
+						<p>1</p>
+						<p>2</p>
+					</Stack>
+				</div>
+				<div className = "flavour">
+
+				</div>
+				<div className = "evoLine">
+
+				</div>
+			</Stack>
+		</Container>
 	);
 };
 export default Details;
@@ -96,3 +118,38 @@ export default Details;
 
 // return displayPokemonHTML;
 // }
+
+{/* <Link to="/">
+					<button>LogoHome</button>
+				</Link>
+				{pokemon ?(
+				<h1> {pokemon.name} </h1>
+
+				<h1></h1>
+				) : (
+									<CircularProgress></CircularProgress>
+								)}
+
+				<h1>Optional Content</h1>
+			</div>
+			<div>
+				<h2>PokemonCard.name</h2>
+			</div>
+			<div>
+				<h2>PokemonCard.img</h2>
+				<h2>Shiny.img</h2>
+			</div>
+			<div>
+				<div>
+					<h3>Type</h3>
+				</div>
+				<div>
+					<h3>Stats</h3>
+				</div>
+				<h3>Moves</h3>
+			</div>
+			<div>
+				<p>Height, Weight, Color, Flavor Text</p>
+			</div>
+			<div>
+				<h2>EvoLine</h2> */}
